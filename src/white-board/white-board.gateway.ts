9f3b5aa0d53b68615fa2041abf1935e1a49f1e26
asanntech <asanoyuuki0523@gmail.com>
2025-09-13 18:13:16 +0900

    feat: emit

--- src/white-board/white-board.gateway.ts
+++ src/white-board/white-board.gateway.ts
@@ -10,9 +10,40 @@ export class WhiteBoardGateway {
     return 'Hello world!'
   }
 
+  @SubscribeMessage('join')
+  async handleJoin(client: Socket, params: { roomId: string }): Promise<void> {
+    console.log('Received join request for room:', params.roomId)
+    await client.join(params.roomId)
+    client.to(params.roomId).emit('join', client.id)
+  }
+
   @SubscribeMessage('drawing')
-  handleDrawing(client: Socket, drawingData: DrawingData): void {
-    console.log('Received drawing data:', drawingData)
-    client.broadcast.emit('drawing', drawingData)
+  handleDrawing(client: Socket, params: { roomId: string; drawings: DrawingData[] }): void {
+    console.log('Received drawing data:', params.drawings)
+    client.to(params.roomId).emit('drawing', params.drawings)
+  }
+
+  @SubscribeMessage('transform')
+  handleTransform(client: Socket, params: { roomId: string; drawings: DrawingData[] }): void {
+    console.log('Received transform data:', params.drawings)
+    client.to(params.roomId).emit('transform', params.drawings)
+  }
+
+  @SubscribeMessage('remove')
+  handleRemove(client: Socket, params: { roomId: string; ids: string[] }): void {
+    console.log('Received remove id:', params.ids)
+    client.to(params.roomId).emit('remove', params.ids)
+  }
+
+  @SubscribeMessage('undo')
+  handleUndo(client: Socket, params: { roomId: string; ids: string[] }): void {
+    console.log('Received undo')
+    client.to(params.roomId).emit('undo', params.ids)
+  }
+
+  @SubscribeMessage('redo')
+  handleRedo(client: Socket, params: { roomId: string; drawings: DrawingData[] }): void {
+    console.log('Received redo')
+    client.to(params.roomId).emit('redo', params.drawings)
   }
 }
