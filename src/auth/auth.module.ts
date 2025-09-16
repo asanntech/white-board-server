fe08ee23b6c0142542f52d8e0e845bc0dce167a1
asanntech <asanoyuuki0523@gmail.com>
2025-09-16 19:34:19 +0900

    refactor: import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
-import { UsersModule } from '@/user/user.module'
+import { UsersModule } from '../user/user.module'
 
 @Module({
   controllers: [AuthController],
