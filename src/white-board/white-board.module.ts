d96ca805aa7a23b8c5de8de05a2cefa6deb5f02e
asanntech <asanoyuuki0523@gmail.com>
2025-10-04 08:29:19 +0900

    feat: S3Service

--- src/white-board/white-board.module.ts
+++ src/white-board/white-board.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { WhiteBoardGateway } from './white-board.gateway'
 import { AuthModule } from '../auth/auth.module'
 import { DynamoDBService } from './dynamodb.service'
+import { S3Service } from './s3.service'
 
 @Module({
   imports: [AuthModule],
-  providers: [WhiteBoardGateway, DynamoDBService],
+  providers: [WhiteBoardGateway, DynamoDBService, S3Service],
   exports: [DynamoDBService],
 })
 export class WhiteBoardModule {}
